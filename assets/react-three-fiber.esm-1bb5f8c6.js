ef93d79063a72f80f35793b7b1c0a976c23ba338
Nikita Thakur <nikitathakur@Nikitas-MacBook-Pro.local>
2026-06-10 11:59:19 -0500

    Updates

--- assets/react-three-fiber.esm-1bb5f8c6.js
+++ assets/react-three-fiber.esm-1bb5f8c6.js
@@ -1,4 +1,4 @@
-import{r as Ne,g as Zg}from"./index-2f3c6cac.js";/**
+import{r as Ne,g as Zg}from"./index-813cd180.js";/**
  * @license
  * Copyright 2010-2023 Three.js Authors
  * SPDX-License-Identifier: MIT
